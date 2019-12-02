d15514f51833c8f89457d0233b1a7c66a14c71e1
Dom Tunstill <datunstill@gmail.com>
2019-12-02 23:41:57 +0000

    changed some routes to correct URL

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -38,7 +38,7 @@ class Signup extends Component {
     if (password === password_confirm) {
       let self = this;
       axios
-        .post("/api/v1/players/new", {
+        .post("/api/api/v1/players/new", {
             email: document.getElementById("email-input").value,
             password: document.getElementById("password-input").value,
             first_name: document.getElementById("first-name-input").value,
